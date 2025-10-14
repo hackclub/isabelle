27e38cb467dec73f7e689b8a754cd30453dc0bcc
Vic <vicxd@pm.me>
2025-10-14 15:01:07 +0200

    specify port

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
 
     sentry_sdk.init(dsn=env.sentry_dsn, traces_sample_rate=1.0)
     sentry_sdk.profiler.start_profiler()
-    uvicorn.run("app:api")
+    uvicorn.run("app:api", port=3000)
