e4c082d8d4e82e6652b03024941c063dcbb6cfe9
Vic <vicxd@pm.me>
2026-04-04 18:17:18 +0200

    uhhh

--- isabelle/utils/database.py
+++ isabelle/utils/database.py
@@ -100,8 +100,8 @@ class DatabaseService:
             if not event:
                 return None
             
-            start_time = updates.get("StartTime", event.StartTime)
-            end_time = updates.get("EndTime", event.EndTime)
+            start_time = updates.get("StartTime", event.get("StartTime")
+            end_time = updates.get("EndTime", event.get("EndTime"))
             updates["CalendarLink"] = make_google_calendar_url(
                 title=updates.get("Title", event.Title),
                 description=updates.get("Description", event.Description),
