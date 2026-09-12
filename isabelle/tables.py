9e2ed66a5006c2e4909b27fe998c42664bb536bc
wally-hc <wally@hackclub.com>
2026-09-12 20:38:24 +0100

    Add a submitter allowlist and record why an event ended

--- isabelle/tables.py
+++ isabelle/tables.py
@@ -38,6 +38,7 @@ class Event(Table):
     SentStartingReminder = Boolean()
     RawDescription = Text(null=True)
     RawCancellation = Text(null=True)
+    CancellationType = Varchar(length=16, null=True)
     # I'm not ready for DB relations and I think a ID's list will work
     # TODO: implement notify by email
     InterestedUsers = Array(base_column=Text(),default=[], secret=True)
@@ -45,3 +46,11 @@ class Event(Table):
     InterestCount = SmallInt() # I know this could easily be calculated but I will try to keep this as close to the airtable as possible
     rsvpMsg = Text(null=True)
     Tags = Array(base_column=Text(), default=[])
+
+
+class Submitter(Table):
+    SlackID = Varchar(length=32, unique=True)
+    Name = Text(null=True)
+    Note = Text(null=True)
+    AddedBySlackID = Varchar(length=32, null=True)
+    AddedAt = Timestamp(null=True)
